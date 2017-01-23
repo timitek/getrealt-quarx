1c96ae27494c74854a5e6530e2258518191add89
Josh Williams <joshua.williams@nephosintegration.com>
2017-01-23 00:38:02 -0600

    Fixed small bug in setEnvironmentValue

--- app/Services/GetRealTService.php
+++ app/Services/GetRealTService.php
@@ -9,29 +9,22 @@ use Artisan;
 class GetRealTService {
     
      /**
-     * Set the application key in the environment file.
+     * Set the in the environment file.
      *
      * @param  string  $key
      * @return void
      */
-    private function setEnvironmentValue($environmentName, $configKey, $newValue) {
-        /*
-            file_put_contents($this->laravel->environmentFilePath(), str_replace(
-                'APP_KEY='.$this->laravel['config']['app.key'],
-                'APP_KEY='.$key,
-                file_get_contents($this->laravel->environmentFilePath())
-            ));
-         */
+    private function setEnvironmentValue($environmentKey, $configKey, $newValue) {
         file_put_contents(App::environmentFilePath(), str_replace(
-            $environmentName . '=' . Config::get($configKey),
-            $environmentName . '=' . $newValue,
+            $environmentKey . '=' . Config::get($configKey),
+            $environmentKey . '=' . $newValue,
             file_get_contents(App::environmentFilePath())
         ));
         
         Config::set($configKey, $newValue);
         
         // Reload the cached config       
-        if (file_exists($cached = App::getCachedConfigPath())) {
+        if (file_exists(App::getCachedConfigPath())) {
             Artisan::call("config:cache");
         }
         
