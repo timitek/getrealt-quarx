f6e7859a5dbc7c76b3edd2dc2264671b6799adea
Josh Williams <joshua.williams@nephosintegration.com>
2017-03-19 16:47:52 -0500

    Added the ability to inject JS and CSS into the admin, added the ability add icons in the Redactor wysiwyg box

--- resources/views/quarx/getrealt/menu.php
+++ resources/views/quarx/getrealt/menu.php
@@ -4,3 +4,5 @@
 <li class="<?= (Request::is('quarx/getrealt/settings') || Request::is('quarx/getrealt/settings/*')) ? 'active' : '' ?>">
     <a href="<?= url('quarx/getrealt/settings') ?>"><span class="fa fa-gear"></span> Settings</a>
 </li>
+<link rel="stylesheet" type="text/css" href="<?= asset('assets/themes/getrealt/css/admin/getrealt-admin.css') ?>">
+<script type="text/javascript" src="<?= asset('assets/themes/getrealt/js/getrealt-admin.js') ?>"></script>
