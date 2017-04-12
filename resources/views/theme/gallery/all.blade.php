468c81d913946b559a2f54cc700418b3125ea483
Josh Williams <joshua.williams@nephosintegration.com>
2017-04-11 22:40:02 -0500

    Matured the image gallery

--- resources/views/theme/gallery/all.blade.php
+++ resources/views/theme/gallery/all.blade.php
@@ -4,17 +4,45 @@
 
 @parallaxHeaderWidget('Gallery', null)
 
-<div class="container">
+<div class="container blog-all">
 
-    <div class="col-md-6">
-        @foreach ($images as $image)
-            <img class="thumbnail img-responsive" alt="{{ $image->alt_tag }}" src="{{ $image->url }}" />
-        @endforeach
-    </div>
-    <div class="col-md-6">
-        @foreach($tags as $tag)
-            <a href="{{ url('gallery/'.$tag) }}" class="btn btn-default">{{ $tag }}</a>
-        @endforeach
+    <div class="row">
+        <div class="col-md-9">
+            <div class="row">
+                @foreach ($images as $key=>$image)
+                <div class="col-md-6">
+                    <div class="panel entry-row">
+                        <div class="panel-heading">
+                            <div class='blog-title'>
+                                @if (!empty($image->title_tag))
+                                <a href="{{ $image->url }}" target="_blank">
+                                    {!! empty($image->title_tag) ? (empty($image->name) ? '[Unnamed]' : $image->name) : $image->title_tag !!} 
+                                </a>
+                                @endif
+                                <div class='published-at'><i class="fa fa-clock-o"></i> {!! \Carbon\Carbon::parse($image->created_at)->format('d M, Y') !!}</div>
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <a href="{{ $image->url }}" target="_blank">
+                                <img class="thumbnail img-responsive" alt="{{ $image->alt_tag }}" src="{{ $image->url }}" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class='tags-header'>
+                <span class='tag-label'><i class='fa fa-tags'></i> Tags</span>
+            </div>
+            <div class="blog-tags">
+                @foreach($tags as $tag)
+                <a href="{{ url('gallery/'.$tag) }}" class="label label-primary tag-{{ rand(1,5) }}">{{ $tag }}</a>
+                @endforeach
+            </div>
+        </div>
     </div>
 
 </div>
--- resources/views/theme/gallery/show.blade.php
+++ resources/views/theme/gallery/show.blade.php
@@ -2,20 +2,39 @@
 
 @section('content')
 
-@parallaxHeaderWidget('Gallery (' . $title . ')', null)
+@parallaxHeaderWidget($title . ' Gallery', null)
 
 <div class="container">
 
-    <div class="col-md-8">
-        @foreach ($images as $image)
-            <img alt="{{ $image->alt_tag }}" src="{{ $image->url }}" />
-        @endforeach
+    @foreach ($images as $image)
+    <div class='row'>
+        <div class='col-xs-12 blog-show'>
+            <div class="body-content">
+                <div class='blog-title'>
+                    @if (!empty($image->title_tag))
+                    <h1>{!! empty($image->title_tag) ? (empty($image->name) ? '[Unnamed]' : $image->name) : $image->title_tag !!}</h1>
+                    @endif
+                    <i class="fa fa-clock-o"></i> Posted on <span class='published-at'>{!! \Carbon\Carbon::parse($image->created_at)->format('d M, Y') !!}</span>
+                </div>
+
+                    <img class="img-responsive" alt="{{ $image->alt_tag }}" src="{{ $image->url }}" />
+
+                @if (!empty($image->name))
+                    <h3>{!! $image->name !!}</h3>
+                @endif
+
+                @if (!empty($image->tags))
+                <div class='blog-footer'>
+                    <span class='footer-label'><i class='fa fa-tags'></i> Tags:</span>
+                    @foreach(explode(',', $image->tags) as $tag)
+                    <a href="{{ url('gallery/'.$tag) }}" class="btn btn-xs btn-default">{{ $tag }}</a>
+                    @endforeach
+                </div>
+            </div>
+            @endif
+        </div>
     </div>
-    <div class="col-md-4">
-        @foreach($tags as $tag)
-            <a href="{{ url('gallery/'.$tag) }}" class="btn btn-default">{{ $tag }}</a>
-        @endforeach
-    </div>
+    @endforeach
 
 </div>
 
