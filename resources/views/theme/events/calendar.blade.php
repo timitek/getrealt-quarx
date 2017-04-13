4709616332b23d77b4f7d31c503f90591262e8a1
Josh Williams <joshua.williams@nephosintegration.com>
2017-04-13 00:16:11 -0500

    Made a mobile option for the calendar view

--- resources/views/theme/events/calendar.blade.php
+++ resources/views/theme/events/calendar.blade.php
@@ -6,9 +6,59 @@
 
 <div class="container">
 
-    <div class="row">
+    <div class="row visible-md visible-lg">
         <div class="col-md-12 calendar-content">
             {!! $calendar->asHtml([ 'class' => 'calendar', 'dates' => $events ]); !!}
+        </div>
+    </div>
+
+    <div class="row visible-xs visible-sm">
+        <div class="col-md-9 blog-all">
+
+            @if ($events && count($events))
+                @foreach($events as $key => $daysEvents)
+                    <div class="panel entry-row">
+                        <div class="panel-heading">
+                            <div class='blog-title'>
+                                <i class="fa fa-calendar"></i> 
+                                <a href="{!! url('events/date/'.$key) !!}">
+                                    {!! \Carbon\Carbon::parse($key)->format('d M, Y') !!}
+                                </a> 
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            @foreach($daysEvents as $event)
+                                <div>
+                                    <a href="{!! url('events/event/'.$event->id) !!}">
+                                        {!! $event->title !!} 
+                                    </a>
+                                    @if ($event->start_date != $event->end_date)
+                                    <span class='published-at'>
+                                        -
+                                        <small>from</small> <strong>{!! \Carbon\Carbon::parse($event->start_date)->format('d M, Y') !!}</strong>
+                                        <small>through</small> <strong>{!! \Carbon\Carbon::parse($event->end_date)->format('d M, Y') !!}</strong>
+                                    </span>                            
+                                    @endif
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <div class="panel entry-row">
+                    <div class="panel-heading">
+                        <div class='blog-title'>
+                            <i class="fa fa-calendar"></i>
+                            No Events At This Time 
+                        </div>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
             {!! $calendar->links('cal-link btn btn-default'); !!}
         </div>
     </div>
